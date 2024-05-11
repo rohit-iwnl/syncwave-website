fde98da5621a9800a29d77323bdbce5649bd3dc9
rohit-iwnl <rohitmanivel9@gmail.com>
2024-05-10 22:00:19 -0700

    animate flower and phone

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,7 @@ export default function Home() {
   return (
     <div className="md:max-w-7xl mx-auto flex flex-col">
       <Hero />
-      <div className="w-full bg-red-200 h-screen">
-
-      </div>
+      <div className="w-full bg-red-200 h-screen"></div>
     </div>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,7 +12,7 @@ export default function Hero({}: Props) {
     <div className=" w-full h-screen max-h-screen flex mt-10 md:mt-14 gap-10 flex-col">
       <div className="flex flex-col gap-5">
         <h1
-          className="text-6xl text-center font-bold bg-clip-text text-transparent"
+          className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center font-bold bg-clip-text text-transparent"
           style={{
             backgroundImage: "linear-gradient(to bottom, #242424, #8A8A8A)",
             WebkitBackgroundClip: "text",
@@ -52,27 +52,53 @@ export default function Hero({}: Props) {
 
       {/* Mockup and background */}
 
-      <motion.div
-        initial={{
-          opacity: 0,
-        }}
-        animate={{
-          opacity: 1,
-        }}
-        transition={{
-          duration: 1.5,
-          delay: 1,
-        }}
-        className="flex-grow relative items-center justify-center overflow-hidden"
-      >
-        <Image
-          src="/assets/images/mockup1.png"
-          alt="Mockup"
-          layout="fill"
-          className="object-cover"
-          quality={100}
-        />
-      </motion.div>
+      <div className="flex flex-col flex-1 items-center justify-center overflow-hidden">
+        <div className="relative w-full h-full flex items-center justify-center">
+          <motion.div
+            initial={{
+              opacity: 0,
+            }}
+            animate={{
+              opacity: 1,
+            }}
+            transition={{
+              duration: 1,
+              ease: "easeIn",
+            }}
+          >
+            <Image
+              src="/assets/images/flower.svg" // Ensure the path is correct
+              alt="mockup"
+              layout="fill" // Takes the full width and height of the parent
+              className="object-cover object-top absolute bottom-0 " // Covers the entire area of the container, possibly cropping the image
+            />
+          </motion.div>
+          <motion.div
+            initial={{
+              opacity: 0,
+            }}
+            animate={{
+              opacity: 1,
+              y: 0,
+            }}
+            transition={{
+              duration: 1,
+              delay: 1.5,
+              ease: "easeIn",
+            }}
+            className="relative w-full h-full flex items-center justify-center"
+          >
+            <Image
+              src="/assets/images/mockup.png"
+              alt="mockup"
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="object-contain absolute w-[60%] h-[100%] "
+            />
+          </motion.div>
+        </div>
+      </div>
     </div>
   );
 }
